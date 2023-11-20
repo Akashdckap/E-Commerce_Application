b5fc437f55ce5c4cddb6f2d9c3d993c7c7ab82a1
Akashdckap <akash.a.dckap@gmail.com>
2023-11-20 15:49:13 +0530

    working on validation

--- server/graphql/resolver.js
+++ server/graphql/resolver.js
@@ -3,6 +3,9 @@ const order = require('../model/order');
 const product = require('../model/product');
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
+// const { graphqlError } = require('graphql')
+const { ApolloError } = require('@apollo/server');
+
 const resolvers = {
     Query: {
         getAllAdmins: async () => {
@@ -21,11 +24,21 @@ const resolvers = {
                 email: email,
                 password: password
             })
-            const res = await newUsers.save();
-
-            return {
-                ...res._doc
+            // console.log(newUsers.email)
+            // async (req, res) => {
+            const adminList = await admins.find({ email: newUsers.email });
+            if (adminList.length > 0) {
+                return {
+                    ...adminList._doc
+                }
             }
+            else {
+                throw new ApolloError(message, {
+                    extensions: { code: 'not exists' },
+                });
+            }
+            // }
+
         },
 
         async createOrders(_, { newOrders: { productId, quantity, name, email, phoneNo, address, district, state, pincode } }) {
