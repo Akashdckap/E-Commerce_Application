1b64656f5628453011e40673cdaeb0b68da90255
Balamurugan R <balamuruganrdckap@gmail.com>
2023-12-12 09:55:15 +0530

    cart funnality completed

--- Client/src/Reducer/productReducer.js
+++ Client/src/Reducer/productReducer.js
@@ -8,10 +8,23 @@ export const productSlice = createSlice({
     reducers: {
         storeAddToCartProductData: (state, action) => {
             const productData = action.payload
+            // console.log("productData---------------", productData);
             if (productData) {
                 productData.forEach(element => {
                     if (!state.cartData.some(item => item._id === element._id)) {
                         state.cartData.push(element)
+                    }
+                    const productIndex = state.cartData.findIndex(product => product._id === element._id)
+                    if (productIndex !== -1) {
+                        if (state.cartData.some(item => item._id === element._id)) {
+                            if (state.cartData[productIndex].count === undefined) {
+                                console.log("no");
+                                state.cartData[productIndex].count = (state.cartData[productIndex].count || 0) + 1;
+                            } else {
+                                console.log("yes");
+                                state.cartData[productIndex].count = 1;
+                            }
+                        }
                     }
                 });
             }
@@ -28,7 +41,7 @@ export const productSlice = createSlice({
         decrementProductCount: (state, action) => {
             const { productId } = action.payload
             const productIndex = state.cartData.findIndex(product => product._id === productId)
-            state.cartData[productIndex].count = Math.max((state.cartData[productIndex].count || 0) - 1, 0);
+            state.cartData[productIndex].count = Math.max((state.cartData[productIndex].count || 0) - 1, 1);
         },
         removeAllCartDatas: (state) => {
             state.cartData = state.cartData = []
