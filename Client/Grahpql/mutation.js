9d28200010d9ec37d5307ec967938dfd43977328
Akashdckap <akash.a.dckap@gmail.com>
2023-12-21 15:26:53 +0530

    mutation for client

--- Client/Grahpql/mutation.js
+++ Client/Grahpql/mutation.js
@@ -57,7 +57,43 @@ export const UPLOAD_FILE = gql`
 
 export const ORDER_PRODUCT = gql`
   mutation createOrders($input: input){
-    createOrders(input: $input)
+    createOrders(input: $input){
+      orderedProducts{
+        productName,
+        category,
+        brand,
+        color,
+        quantity,
+        price
+      },
+      personalDetails{
+        name,
+        email,
+        phoneNo
+      },
+      shippingAddress{
+        firstName,
+        lastName,
+        email,
+        phoneNo,
+        address,
+        district,
+        state,
+        pincode,
+        country
+      }
+      billingAddress{
+        firstName,
+        lastName,
+        email,
+        phoneNo,
+        address,
+        district,
+        state,
+        pincode,
+        country
+      }
+    }
   }
 `
 
